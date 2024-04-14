cd748d3f18ceed1510e04d8ffc5836c0d77d0fbd
razashahzad26 <135547515+razashahzad26@users.noreply.github.com>
2024-04-15 01:20:13 +0500

    Update views.py

--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 core_bp = Blueprint("core", __name__)
+
+@core_bp.route("/")
+@login_required
+def home():
+    return render_template("core/index.html")
