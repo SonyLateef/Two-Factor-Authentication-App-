24a37b98e1ad9310006f4f2056ad3202825768d5
razashahzad26 <135547515+razashahzad26@users.noreply.github.com>
2024-04-15 01:27:00 +0500

    Create forms.py

---
+++ src/accounts/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+from src.accounts.models import User
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=6, max=25)]
+    )
+    confirm = PasswordField(
+        "Repeat password",
+        validators=[
+            DataRequired(),
+            EqualTo("password", message="Passwords must match."),
+        ],
+    )
+
+    def validate(self, extra_validators):
+        initial_validation = super(RegisterForm, self).validate(extra_validators)
+        if not initial_validation:
+            return False
+        user = User.query.filter_by(username=self.username.data).first()
+        if user:
+            self.username.errors.append("Username already registered")
+            return False
+        if self.password.data != self.confirm.data:
+            self.password.errors.append("Passwords must match")
+            return False
+        return True
