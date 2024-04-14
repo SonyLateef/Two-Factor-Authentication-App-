9eea4eddb62ed40e020947daf752a31bcd6eba41
razashahzad26 <135547515+razashahzad26@users.noreply.github.com>
2024-04-15 01:35:10 +0500

    Update views.py

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,29 @@
 from flask import Blueprint
+from .forms import LoginForm, RegisterForm
 
 accounts_bp = Blueprint("accounts", __name__)
+@accounts_bp.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        if current_user.is_two_factor_authentication_enabled:
+            flash("You are already logged in.", "info")
+            return redirect(url_for(HOME_URL))
+        else:
+            flash("You have not enabled 2-Factor Authentication. Please enable first to login.", "info")
+            return redirect(url_for(SETUP_2FA_URL))
+        
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, request.form["password"]):
+            login_user(user)
+            if not current_user.is_two_factor_authentication_enabled:
+                flash(
+                    "You have not enabled 2-Factor Authentication. Please enable first to login.", "info")
+                return redirect(url_for(SETUP_2FA_URL))
+            return redirect(url_for(VERIFY_2FA_URL))
+        elif not user:
+            flash("You are not registered. Please register.", "danger")
+        else:
+            flash("Invalid username and/or password.", "danger")
+    return render_template("accounts/login.html", form=form)
