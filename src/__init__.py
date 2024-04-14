00fd88cf7a81c76352cb50a4bc82cea426128edd
Yunus Kocaman <156374474+ykunus@users.noreply.github.com>
2024-04-14 14:14:43 -0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,14 @@
 from decouple import config
 from flask import Flask
-from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(config("APP_SETTINGS"))
-
-bcrypt = Bcrypt(app)
+#create and initilize login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -17,3 +18,12 @@ from src.core.views import core_bp
 
 app.register_blueprint(accounts_bp)
 app.register_blueprint(core_bp)
+
+from src.accounts.models import User
+
+#reloaddsthe user object from the user ID stored
+#takes ID of user and returns corresponding User object
+@login_manager.user_loader
+def load_user(user_id):
+  return user.query.filter(User.id == int(user_id)).first()
+
