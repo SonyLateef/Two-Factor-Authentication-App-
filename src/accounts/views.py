e0d61f7674c89291a460b8ee1c9ec450f30249e7
SonyLateef <91687027+SonyLateef@users.noreply.github.com>
2024-05-09 13:08:08 -0400

    views changes

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,17 +1,45 @@
-from flask import Blueprint, redirect, render_template, request, url_for, flash
-from .forms import LoginForm, RegisterForm
-from flask_login import login_required, login_user, logout_user, current_user
 from src.utils import get_b64encoded_qr_image
-from models import User
-from forms import TwoFactorForm
-from __init__ import db
-import bcrypt
+from .forms import LoginForm, RegisterForm, TwoFactorForm
+from src.accounts.models import User
+from src import db, bcrypt
+from flask_login import current_user, login_required, login_user, logout_user
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+
+accounts_bp = Blueprint("accounts", __name__)
 
 HOME_URL = "core.home"
 SETUP_2FA_URL = "accounts.setup_two_factor_auth"
 VERIFY_2FA_URL = "accounts.verify_two_factor_auth"
 
-accounts_bp = Blueprint("accounts", __name__)
+
+@accounts_bp.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        if current_user.is_two_factor_authentication_enabled:
+            flash("You are already registered.", "info")
+            return redirect(url_for(HOME_URL))
+        else:
+            flash(
+                "You have not enabled 2-Factor Authentication. Please enable first to login.", "info")
+            return redirect(url_for(SETUP_2FA_URL))
+    form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        try:
+            user = User(username=form.username.data, password=form.password.data)
+            db.session.add(user)
+            db.session.commit()
+
+            login_user(user)
+            flash("You are registered. You have to enable 2-Factor Authentication first to login.", "success")
+
+            return redirect(url_for(SETUP_2FA_URL))
+        except Exception:
+            db.session.rollback()
+            flash("Registration failed. Please try again.", "danger")
+
+    return render_template("accounts/register.html", form=form)
+
+
 @accounts_bp.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
@@ -19,7 +47,8 @@ def login():
             flash("You are already logged in.", "info")
             return redirect(url_for(HOME_URL))
         else:
-            flash("You have not enabled 2-Factor Authentication. Please enable first to login.", "info")
+            flash(
+                "You have not enabled 2-Factor Authentication. Please enable first to login.", "info")
             return redirect(url_for(SETUP_2FA_URL))
         
     form = LoginForm(request.form)
@@ -38,6 +67,7 @@ def login():
             flash("Invalid username and/or password.", "danger")
     return render_template("accounts/login.html", form=form)
 
+
 @accounts_bp.route("/logout")
 @login_required
 def logout():
@@ -46,9 +76,9 @@ def logout():
     return redirect(url_for("accounts.login"))
 
 
-@accounts_bp.route("setup/2fa")
+@accounts_bp.route("/setup-2fa")
 @login_required
-def setup_two_factor():
+def setup_two_factor_auth():
     secret = current_user.secret_token
     uri = current_user.get_authentication_setup_uri()
     base64_qr_image = get_b64encoded_qr_image(uri)
