f1813540da76c518af1b2671f6f29f9a3950597f
SonyLateef <91687027+SonyLateef@users.noreply.github.com>
2024-05-09 13:27:56 -0400

    test

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from .accounts.models import user  # Keep this import
+from accounts.models import user
 
 app = Flask(__name__)
 app.config.from_object(config("APP_SETTINGS"))
